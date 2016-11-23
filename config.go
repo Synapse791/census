2a5222c13e9fda7f55b80f2af2a731113cc42c9f
Iain Earl <synapse791@gmail.com>
2016-11-23 12:53:19 +0000

    Adding etcd prefix to config

--- config.go
+++ config.go
@@ -10,8 +10,9 @@ const CONFIG_PATH = "/etc/census/config.yml"
 
 type CensusConfig struct {
   Etcd  struct {
-    Host  string  `yaml:"host"`
-    Port  int     `yaml:"port"`
+    Host    string  `yaml:"host"`
+    Port    int     `yaml:"port"`
+    Prefix  string  `yaml:"prefix"`
   } `yaml:"etcd"`
 }
 
@@ -32,13 +33,17 @@ func LoadConfig() *CensusConfig {
   }
 
   if appConfig.Etcd.Host == "" {
-    console.Warning("etcd host is not defined in %s.             Using default: 127.0.0.1", CONFIG_PATH)
+    console.Warning("etcd host is not defined in %s.               Using default: \"127.0.0.1\"", CONFIG_PATH)
     appConfig.Etcd.Host = "127.0.0.1"
   }
 
   if appConfig.Etcd.Port == 0 {
-    console.Warning("etcd port is not defined in %s or set to 0. Using default: 2379", CONFIG_PATH)
+    console.Warning("etcd port is not defined in %s or set to 0.   Using default: 2379", CONFIG_PATH)
     appConfig.Etcd.Port = 2379
+  }
+
+  if appConfig.Etcd.Prefix == "" {
+    console.Warning("etcd prefix is not defined in %s.             Using default: \"\"", CONFIG_PATH)
   }
 
   return &appConfig
