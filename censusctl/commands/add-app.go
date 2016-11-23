bd81ff2d9e7f4803f016440b7f05a05f96c06080
Iain Earl <synapse791@gmail.com>
2016-11-23 18:53:53 +0000

    Adding working add app command

--- censusctl/commands/add-app.go
+++ censusctl/commands/add-app.go
@@ -1,10 +1,61 @@
 package commands
 
 import (
+  "fmt"
+  "net/http"
+  "time"
+  "bytes"
+  "encoding/json"
+  "io/ioutil"
   "github.com/urfave/cli"
+  "github.com/Synapse791/census/server"
 )
 
 func AddApp(c *cli.Context) error {
-  println("this will add a new app")
+  if c.String("name") == "" {
+    return cli.NewExitError("The name flag is required", 1)
+  }
+
+  if c.String("port") == "" {
+    return cli.NewExitError("The port flag is required", 1)
+  }
+
+  netClient := &http.Client{
+    Timeout: time.Second * 10,
+  }
+
+  payload := server.PutAppRequest{}
+  payload.Name = c.String("name")
+  payload.Port = c.String("port")
+
+  jsonPayload, jsonErr := json.Marshal(payload)
+  if jsonErr != nil {
+    return cli.NewExitError(jsonErr.Error(), 1)
+  }
+
+  req, reqErr := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/apps", c.GlobalString("server-address")), bytes.NewBuffer(jsonPayload))
+  if reqErr != nil {
+    return cli.NewExitError(reqErr.Error(), 1)
+  }
+
+  req.Header.Set("Content-Type", "application/json")
+
+  response, resErr := netClient.Do(req)
+  if resErr != nil {
+    return cli.NewExitError(resErr.Error(), 1)
+  }
+  defer response.Body.Close()
+
+  body, readErr := ioutil.ReadAll(response.Body)
+  if readErr != nil {
+    return cli.NewExitError(readErr.Error(), 1)
+  }
+
+  if response.StatusCode != 201 {
+    return cli.NewExitError(fmt.Sprintf("Failed to create app: %s", string(body)), 1)
+  }
+
+  fmt.Println("App created successfully")
+
   return nil
 }
