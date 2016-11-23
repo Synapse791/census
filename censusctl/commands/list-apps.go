51a95258507def41153a1ea7dfcc0d9d757dae4b
Iain Earl <synapse791@gmail.com>
2016-11-23 18:52:35 +0000

    Adding working list apps command. Needs nicer output format

--- censusctl/commands/list-apps.go
+++ censusctl/commands/list-apps.go
@@ -1,10 +1,44 @@
 package commands
 
 import (
+  "fmt"
+  "net/http"
+  "time"
+  "encoding/json"
+  "io/ioutil"
   "github.com/urfave/cli"
 )
 
 func ListApps(c *cli.Context) error {
-  println("this will list apps and their ports")
+  netClient := &http.Client{
+    Timeout: time.Second * 10,
+  }
+
+  response, resErr := netClient.Get(fmt.Sprintf("%s/apps", c.GlobalString("server-address")))
+  if resErr != nil {
+    return cli.NewExitError(resErr.Error(), 1)
+  }
+  defer response.Body.Close()
+
+  body, readErr := ioutil.ReadAll(response.Body)
+  if readErr != nil {
+    return cli.NewExitError(readErr.Error(), 1)
+  }
+
+  if response.StatusCode != 200 {
+    return cli.NewExitError(fmt.Sprintf("Failed to get app list: %s", string(body)), 1)
+  }
+
+  var decodedRepsonse map[string]string
+
+  jsonErr := json.Unmarshal(body, &decodedRepsonse)
+  if jsonErr != nil {
+    return cli.NewExitError(jsonErr.Error(), 1)
+  }
+
+  for appName, appPort := range decodedRepsonse {
+    fmt.Printf("  Name: %s, Port: %s\n", appName, appPort)
+  }
+
   return nil
 }
--- censusctl/main.go
+++ censusctl/main.go
@@ -10,35 +10,63 @@ func main() {
   app := cli.NewApp()
   app.Name = "censusctl"
   app.Usage = "Census Control"
+  app.Version = "1.0"
+  app.Flags = []cli.Flag{
+    cli.StringFlag{
+      Name: "server-address, s",
+      Usage: "Address of the census server",
+      Value: "http://localhost:3100",
+    },
+  }
   app.Commands = []cli.Command{
-    {
+    cli.Command{
       Name: "list",
       Usage: "Lists resources of given type",
       Subcommands: []cli.Command{
-        {
+        cli.Command{
           Name: "apps",
           Usage: "Lists apps in the census",
           Action: commands.ListApps,
         },
-        {
+        cli.Command{
           Name: "hosts",
           Usage: "Lists hosts in the census",
           Action: commands.ListHosts,
         },
       },
     },
-    {
+    cli.Command{
       Name: "add",
       Usage: "Adds a new resource of the given type",
       Subcommands: []cli.Command{
-        {
+        cli.Command{
           Name: "app",
           Usage: "Adds a new app to the census",
+          Flags: []cli.Flag{
+            cli.StringFlag{
+              Name: "name, n",
+              Usage: "The name for the new app",
+            },
+            cli.StringFlag{
+              Name: "port, p",
+              Usage: "The port for the new app",
+            },
+          },
           Action: commands.AddApp,
         },
-        {
+        cli.Command{
           Name: "host",
           Usage: "Adds a new host to the census",
+          Flags: []cli.Flag{
+            cli.StringFlag{
+              Name: "name, n",
+              Usage: "The name for the new host",
+            },
+            cli.StringFlag{
+              Name: "ip-address, i",
+              Usage: "The ip address for the new host",
+            },
+          },
           Action: commands.AddHost,
         },
       },
